06464b94770f402430c5f24d1a4cbd3c75aec1c1
Ben Firth <bfirth93@gmail.com>
2020-10-19 15:21:12 -0600

    Make chart dynamic through props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,18 @@ import Chart from '../../d3files/d3file';
 import './App.css';
 
 const App = () => {
+    const dataOne = [12,36,6,25,35,10,40];
+    const dataTwo = [45,73,6,14];
+    const dataThree = [1,16,6,25,73,6,43,36,20,10];
+
+    const width = 500;
+    const height = 500;
 
   return (
     <main>
-      <Chart />
+      <Chart data={dataOne} w={width} h={height} color='cyan' />
+      <Chart data={dataTwo} w={300} h={height} color='yellow' />
+      <Chart data={dataThree} w={700} h={height} color='rebeccapurple' />
     </main>
   );
 }
--- src/d3files/d3file.js
+++ src/d3files/d3file.js
@@ -1,14 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
-const Chart = ({ data, height, width}) => {
+const Chart = ({ data, h, w, color }) => {
   const myRef = useRef();
 
   const drawChart = () => {
-    const testData = [12,36,6,25,35,10,40];
-    const w = 500;
-    const h = 400;
-  
     const accessToRef = d3.select(myRef.current)
       .append('svg')
       .attr('width', w)
@@ -18,14 +14,14 @@ const Chart = ({ data, height, width}) => {
       .style('margin-left', 50)
   
     accessToRef.selectAll('rect')
-      .data(testData)
+      .data(data)
       .enter()
       .append('rect')
       .attr('x', (d, i) => i * 70)
       .attr('y', (d, i) => h - 10 * d)
       .attr('width', 65)
       .attr('height', (d, i) => d * 10)
-      .attr('fill', (d, i) => d > 25 ? 'tomato' : 'yellow')
+      .attr('fill', (d, i) => d > 25 ? color : 'tomato')
   };
 
   useEffect(() => {
